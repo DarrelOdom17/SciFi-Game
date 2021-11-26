a3b24ef5cc11c28c361acf817a55d029e271f5c8
Darrel Odom <60436595+DarrelOdom17@users.noreply.github.com>
2021-11-26 15:13:00 -0600

    Added new project

---
+++ SciFi Project/Assets/Scripts/Gun.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float impactForce = 30f;
+    public float fireRate = 15f;
+    public bool autoFire;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            autoFire = true;
+        }
+
+        if (Input.GetButtonDown("Fire1") && autoFire == false)
+        {
+            Shoot();
+        }
+
+        if (Input.GetButtonDown("Fire1") && autoFire == true && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+void Shoot()
+    {
+        muzzleFlash.Play();
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+           Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+        }
+    }
+
+    void ToggleShoot()
+    {
+        if(Input.GetKey(KeyCode.Alpha1))
+        {
+            autoFire = !autoFire;
+        }
+    }
+}
